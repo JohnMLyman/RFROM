8d078d9b97152520d9cd7c2850579d4242ee8f92
JohnMLyman <38117446+JohnMLyman@users.noreply.github.com>
2025-05-29 12:26:45 -0700

    ceres

--- OHU_CERESl.m
+++ OHU_CERESl.m
@@ -1,4 +1,4 @@
-function OHU_curve_and_ascii_file(TreeSetUp,min_depth,max_depth)
+function [rate_ceres,tgrid_ceres]=OHU_CERESl(TreeSetUp)
 % TreeSetUp=TreeSetUp_2024_orca_heat_novert_test_mid;
 
 end_year=TreeSetUp.end_year;
@@ -7,22 +7,20 @@ tree_prefix=TreeSetUp.tree_prefix;
 OUTOUT_type=TreeSetUp.OUTOUT_type;
 
 
-depth_range_name=[num2str(min_depth),'_',num2str(max_depth)];
-
-curve_name=['curve_',tree_prefix,'_',depth_range_name,'_ohca_nomean_',OUTOUT_type,'.mat'];
-file_new=[path_Fig_data,curve_name];
-
-file_txt=[path_Fig_data,'RFROMv22_OHU_',num2str(end_year.*10),'.txt'];
 
 
-load(file_new,'tgrid','ht_curve')
 
-tgrid_all=double(tgrid);
-ht_curve_all=double(ht_curve);
+file_txt=[path_Fig_data,'RFROMv22_OHU_',num2str(end_year.*10),'_junk.txt'];
+
+files_ceres='C:\Users\jlyma\Downloads\ceres_ohca.mat'
+load(files_ceres,'yr_ceres_ohca','ceres_ohca')
+tgrid_all=yr_ceres_ohca';
+tgrid=tgrid_all;
+ht_curve_all=ceres_ohca*1e22;
 
  ht_tree_all=double(ht_curve_all);
  time_tree_all=double(tgrid_all);
-good_fit=time_tree_all>=2010 & time_tree_all<=2022;
+good_fit=time_tree_all>=2010 & time_tree_all<=2021;
 
  center_year_all=mean(time_tree_all(good_fit));
 
@@ -40,31 +38,42 @@ ht_all_no_cycle=ht_tree_all_res'+(time_tree_all-center_year_all).*slope_tree_all
 area_of_earth=5.1e14;
 
 ndays=365.25;
+ndays_month=ndays/12.;
 
 sec_in_day=(60.*60*24);
 fac_year=1./(sec_in_day*area_of_earth.*ndays);
+fac_month=1./(sec_in_day*area_of_earth.*ndays_month);
+
 % fac_day=1./(sec_in_day*area_of_earth.*1);
 
 
 
 tgrid_12mon=[(1993.5-1/24):1./12.:end_year-1/24];
 ht_12mon=nans(1,length(tgrid_12mon));
+ht_1mon=ht_12mon;
 % ht_12mon_nofilt=nans(1,length(tgrid_12mon));
+
+
 for iyear=tgrid_12mon
 
     good=tgrid>=iyear-.5 & tgrid<=iyear+.5;
+    good_month=tgrid>=iyear-1/24 & tgrid<=iyear+1/24;
   
     
 
     ht_12mon(tgrid_12mon==iyear)=mean(ht_all_no_cycle(good),'omitnan');
+    ht_1mon(tgrid_12mon==iyear)=mean(ht_all_no_cycle(good_month),'omitnan');
 %     ht_12mon_nofilt(tgrid_12mon==iyear)=mean(ht_tree_all(good),'omitnan');
 end
 
 
 rate_12mon=-1.*(ht_12mon(1:end-12)-ht_12mon(13:end)).*fac_year;
+rate_1mon=-1.*(ht_12mon(1:end-5)-ht_12mon(6:end)).*fac_month;
+rate_12mon_s=-1.*(ht_1mon(1:end-12)-ht_1mon(13:end)).*fac_year;
 % rate_12mon_nofilt=-1.*(ht_12mon_nofilt(1:end-12)-ht_12mon_nofilt(13:end)).*fac_year;
 
 tgrid_rate_12mon=(tgrid_12mon(1:end-12)+tgrid_12mon(13:end))./2;
+tgrid_rate_1mon=(tgrid_12mon(1:end-4)+tgrid_12mon(5:end))./2;
 
 
 good_tree=tgrid_rate_12mon>=2005.5;
@@ -73,11 +82,12 @@ rate=rate_12mon(good_tree);
 
 
 
+tgrid_ceres=tgrid_rate;
+rate_ceres=rate;
 
 
 
 
 
-
-
-write_ascii_CERES_RFROM_new(rate',tgrid_rate',file_txt)
+% 
+% write_ascii_CERES_RFROM_new(rate',tgrid_rate',file_txt)
